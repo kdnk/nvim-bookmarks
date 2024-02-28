41dfacb980a6e581f976ea6b9f5e204b0dee230e
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2024-02-28 18:01:37 +0900

    chore: per-branch

--- lua/bookmarks/persist.lua
+++ lua/bookmarks/persist.lua
@@ -5,11 +5,27 @@ local sync = require("bookmarks.sync")
 
 local M = {}
 
+local function escape_branch(branch)
+    local escaped_branch = branch:gsub("([ %(%)%&%$%#%{%}%[%]%*%?%'\"\\])", "\\%1")
+    escaped_branch = escaped_branch:gsub("%s", "\\ ")
+    escaped_branch = escaped_branch:gsub("/", "+")
+    return escaped_branch
+end
+
+local function persist_path()
+    local branch = vim.fn.systemlist("git branch --show-current")[1] or ""
+    if config.persist.per_branch then
+        return config.persist.path .. "-" .. escape_branch(branch) .. ".json"
+    else
+        return config.persist.path .. ".json"
+    end
+end
+
 function M.backup()
     if config.persist.enable then
         bookmark.update_bufnr()
         local json = bookmark.toJson()
-        file.json_write(json, config.persist.path)
+        file.json_write(json, persist_path())
     end
 end
 
@@ -23,7 +39,7 @@ function M.restore()
     end
 
     vim.schedule(function()
-        local json = file.json_read(config.persist.path)
+        local json = file.json_read(persist_path())
         local bookmarks = bookmark.fromJson(json)
         bookmark.update_all(bookmarks)
 
